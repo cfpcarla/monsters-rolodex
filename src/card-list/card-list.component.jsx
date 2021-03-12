4b9fa179229c2434ca5d2ca656ec150a83ac0757
Carla <cfpcarla@gmail.com>
2021-03-12 15:19:37 -0500

    Changed state for props

--- src/card-list/card-list.component.jsx
+++ src/card-list/card-list.component.jsx
@@ -4,7 +4,7 @@ import './card-list.styles.css'
 
  export const CardList = (props) => {
 
-    return <div className= 'card-list'> {props.children} </div>
+    return <div className= 'card-list'> {
+        props.monsters.map(monsters => <h1 key={monsters.id}> {monsters.name} </h1>)
+      } </div>
  }
-
-//  export default CardList;
